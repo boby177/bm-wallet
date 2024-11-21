73c0aea5f140730d50c6d188388e2cd6e4405a93
Boby M <boby.s@kiranatama.com>
2024-11-21 11:01:19 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}.`);
+});
